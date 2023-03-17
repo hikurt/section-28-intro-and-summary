96914c8b9c27f42c9ff7d031e1d3203893da0aed
hikurt <32220244+hikurt@users.noreply.github.com>
2023-03-17 12:06:22 +0100

    Added removing Item

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,10 +4,16 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({title: '', amount: ''});
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    
+    props.onAddIgredient({
+      title: enteredTitle,
+      amount: enteredAmount
+    });
   };
 
   return (
@@ -19,13 +25,10 @@ const IngredientForm = React.memo(props => {
             <input 
               type="text"
               id="title"
-              value={inputState.title} 
-              onChange={event => 
-                setInputState((prevInputState) => {
-                  const newTitle = event.target.value;
-                  ({title: newTitle, amount: prevInputState.amount}
-                )})
-              }
+              value={enteredTitle} 
+              onChange={event => {
+                setEnteredTitle(event.target.value);
+              }}
             />
           </div>
           <div className="form-control">
@@ -33,13 +36,10 @@ const IngredientForm = React.memo(props => {
             <input 
               type="number" 
               id="amount" 
-              value={inputState.amount}
+              value={enteredAmount}
               onChange={event => {
-                const newAmount = event.target.value;
-                setInputState((prevInputState) => (
-                  {amount: newAmount, title: prevInputState.title}
-              ))}
-              }
+                setEnteredAmount(event.target.value);
+              }}
             />
           </div>
           <div className="ingredient-form__actions">
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients( prevIngredients => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
+
+  const removeItemHandler = (id) => {
+    setUserIngredients( prevIngredients => prevIngredients.filter(p => p.id !== id));
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIgredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeItemHandler}/>
       </section>
     </div>
   );
